8dd4cf80409e625d2a0fbc35b7dcf69a6d713bdf
Stephen Radford <steve228uk@gmail.com>
2022-11-03 01:05:33 +0000

    Add ability to disable IPV6

--- main.py
+++ main.py
@@ -12,6 +12,23 @@ logger.setLevel(logging.INFO)
 # Run the install script for the OpenVPN extension
 subprocess.run(["bash", os.path.dirname(__file__) + "/extensions/install"], cwd=os.path.dirname(__file__) + "/extensions")
 
+def connection_mapper(xn):
+    components = xn.split()
+    return {
+        "name": components[0],
+        "uuid": components[1],
+        "type": components[2],
+        "device": components[3],
+        "connected": False if components[3] == '--' else True
+    }
+
+def get_active_connection():
+    result = subprocess.run(["nmcli", "connection", "show", "--active"], text=True, capture_output=True).stdout
+    connections = result.splitlines()
+    connections.pop(0)
+    mapped = map(connection_mapper, connections)
+    return next(filter(lambda xn: xn["type"] == 'wifi' or xn["type"] == 'ethernet', mapped), None)
+
 class Plugin:
 
     # Lists the connections from network manager.
@@ -19,18 +36,8 @@ class Plugin:
     async def show(self):
         result = subprocess.run(["nmcli", "connection", "show"], text=True, capture_output=True).stdout
         connections = result.splitlines()
-        connections.pop(0)
-
-        def mapper(xn):
-            components = xn.split()
-            return {
-                "name": components[0],
-                "uuid": components[1],
-                "type": components[2],
-                "connected": False if components[3] == '--' else True
-            }
-
-        mapped = map(mapper, connections)
+        connections.pop(0)        
+        mapped = map(connection_mapper, connections)
         return list(mapped)
 
     # Establishes a connection to a VPN
@@ -45,7 +52,39 @@ class Plugin:
         result = subprocess.run(["nmcli", "connection", "down", uuid], text=True, capture_output=True).stdout
         return result
 
+    # Checks if IPV6 is disabled on Wi-Fi
+    async def active_connection(self):
+        connection = get_active_connection()
+        if connection == None:
+            return None
+
+        result = subprocess.run(["nmcli", "connection", "show", connection["uuid"], "|", "grep", "ipv6.metho"], text=True, capture_output=True).stdout
+        connection["ipv6_disabled"] = True if "disabled" in result else False
+        return connection
+
+    # Disables IPV6 on currently active connection
+    async def disable_ipv6(self):
+        connection = get_active_connection()
+        if connection == None:
+            return True
+        
+        logger.info("DISABLING IPV6 for: " + connection["uuid"])
+        subprocess.run(["nmcli", "connection", "modify", connection["uuid"], "ipv6.method", "disabled"])
+        subprocess.run(["systemctl", "restart", "NetworkManager"])
+        return True
+
+    # Enable IPV6 on currently active connection
+    async def enable_ipv6(self):
+        connection = get_active_connection()
+        if connection == None:
+            return True
+        
+        logger.info("ENABLING IPV6 for: " + connection["uuid"])
+        subprocess.run(["nmcli", "connection", "modify", connection["uuid"], "ipv6.method", "auto"])
+        subprocess.run(["systemctl", "restart", "NetworkManager"])
+        return True
+
     # Clean-up on aisle 5
     async def _unload(self):
-        subprocess.run(["bash", os.path.dirname(__file__) + "/extensions/uninstall"])
+        subprocess.run(["bash", os.path.dirname(__file__) + "/extensions/uninstall"], cwd=os.path.dirname(__file__) + "/extensions")
         pass
--- src/index.tsx
+++ src/index.tsx
@@ -21,25 +21,47 @@ type Connection = {
   name: string,
   uuid: string,
   type: string,
-  connected: boolean
+  connected: boolean,
+  ipv6_disabled?: boolean
 }
 
 const Content: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
 
   const [ loaded, setLoaded ] = useState(false);
   const [ connections, setConnections ] = useState<Connection[]>([]);
+  const [ activeConnection, setActiveConnection ] = useState<Connection>();
+  const [ ipv6Disabled, setIpv6Disabled ] = useState(false);
 
   const loadConnections = async () => {
-    const response = await serverAPI.callPluginMethod<{}, Connection[]>('show', {});
-    const connections = response.result as Connection[];
-    const filtered = connections
-    .filter((connection) =>['vpn', 'wireguard'].includes(connection.type))
-    .sort((a, b) => {
-      if(a.name < b.name) return -1;
-      if(a.name > b.name) return 1;
-      return 0;
-    });
-    setConnections(filtered);
+
+    try {
+      const response = await serverAPI.callPluginMethod<{}, Connection[]>('show', {});
+      const connections = response.result as Connection[];
+      const filtered = connections
+      .filter((connection) => ['vpn', 'wireguard'].includes(connection.type))
+      .sort((a, b) => {
+        if(a.name < b.name) return -1;
+        if(a.name > b.name) return 1;
+        return 0;
+      });
+
+      setConnections(filtered);
+    } catch (error) {
+      console.error(error);
+    }
+
+    try {
+      const activeConnectionResponse = await serverAPI.callPluginMethod<{}, Connection>('active_connection', {});
+      const activeConnection = activeConnectionResponse.result as Connection;
+
+      console.log(activeConnectionResponse);
+
+      setActiveConnection(activeConnection);
+      setIpv6Disabled((activeConnection.ipv6_disabled) ? true : false);
+    } catch (error) {
+      console.error(error);
+    }
+
     setLoaded(true);
   }
 
@@ -47,37 +69,55 @@ const Content: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
     await serverAPI.callPluginMethod((switchValue) ? 'up' : 'down', { uuid: connection.uuid });
   }
 
+  const toggleIpv6 = async(switchValue: boolean) => {
+    setIpv6Disabled(switchValue);
+    await serverAPI.callPluginMethod((switchValue) ? 'disable_ipv6' : 'enable_ipv6', {});
+  }
+
   useEffect(() => {
     loadConnections();
   }, []);
 
   return (
-    <PanelSection title="Connections">
+    <>
+      <PanelSection title="Connections">
 
-      {loaded && connections.length == 0 && <PanelSectionRow>
-        No Connections Found
-      </PanelSectionRow>}
+        {loaded && connections.length == 0 && <PanelSectionRow>
+          No Connections Found
+        </PanelSectionRow>}
 
-      {connections.length > 0 && connections.map((connection) => (
+        {connections.length > 0 && connections.map((connection) => (
+          <PanelSectionRow>
+            <ToggleField
+            bottomSeparator='standard'
+            checked={connection.connected}
+            label={connection.name}
+            description={`Type: ${connection.type}`}
+            onChange={(switchValue: boolean) => {
+              toggleConnection(connection, switchValue);
+            }} />
+          </PanelSectionRow>
+        ))}
+
         <PanelSectionRow>
-          <ToggleField
-          bottomSeparator='standard'
-          checked={connection.connected}
-          label={connection.name}
-          description={`Type: ${connection.type}`}
-          onChange={(switchValue: boolean) => {
-            toggleConnection(connection, switchValue);
-          }} />
+          <ButtonItem onClick={() => Router.NavigateToExternalWeb('https://github.com/steve228uk/TunnelDeck#readme')}>
+            How Do I Add Connections?
+          </ButtonItem>
         </PanelSectionRow>
-      ))}
 
-      <PanelSectionRow>
-        <ButtonItem onClick={() => Router.NavigateToExternalWeb('https://github.com/steve228uk/TunnelDeck#readme')}>
-          How Do I Add Connections?
-        </ButtonItem>
-      </PanelSectionRow>
-
-    </PanelSection>
+      </PanelSection>
+      <PanelSection title="Settings">
+          <PanelSectionRow>
+            <ToggleField
+            bottomSeparator='standard'
+            checked={ipv6Disabled}
+            label='Disable IPV6'
+            disabled={!activeConnection}
+            description='Disables IPV6 support for the current connection. Required for some VPNs.'
+            onChange={toggleIpv6} />
+          </PanelSectionRow>
+      </PanelSection>
+    </>
   );
 };
 
